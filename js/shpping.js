d7941d13df2154a55e6cb210e453e4b4bb6fcc7f
shukumar-542 <shukumar542@gmail.com>
2023-03-09 13:06:30 +0600

    total price and total product option done

--- js/shpping.js
+++ js/shpping.js
@@ -8,10 +8,12 @@ const loadProducts =()=>{
 loadProducts();
 
 const showProducts =(products)=>{
+    // console.log(products.length);
     
     products.forEach(product =>{
-        const {title,image,description} = product
-        console.log(product);
+        // console.log(product);
+        const {title,image,description,price} = product
+      
         const productContainer = document.getElementById('product-container')
         productContainer.innerHTML +=`
         <div class="col">
@@ -19,15 +21,24 @@ const showProducts =(products)=>{
           <img src="${image}" class="card-img-top" alt="...">
           <div class="card-body">
             <h5 class="card-title">${title}</h5>
+            <p>Price : ${price}</p>
             <p class="card-text">${description.slice(0,100)}...</p>
-            <button class="btn btn-primary">Add To Cart</button>
+            <button class="btn btn-primary" onclick='addToCart(${price})'>Add To Cart</button>
             <button class="btn btn-success">Details</button>
           </div>
         </div>
       </div>
         `
     })
-//    for(const product of products){
-//     console.log(product);
-//    }
+}
+let count = 0
+const addToCart=(price)=>{
+   
+     count  =  count + 1
+    document.getElementById('total-Products').innerText = count;
+    const element = document.getElementById('price').innerText;
+    const convertedPrice = parseFloat(element)
+    const totalPrice = convertedPrice + price
+    document.getElementById('price').innerText= totalPrice.toFixed(2)
+    console.log( totalPrice);
 }
